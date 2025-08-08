90db3c017d0ed8ab7f2637c4396cf2827ef5f73f
kar7mp5 <tommy1005a@gmail.com>
2025-08-08 09:53:12 +0900

    chore: Delete unusal files

--- include/Lexer/Token.h
+++ include/Lexer/Token.h
@@ -2,35 +2,33 @@
 #include <string>
 #include <vector>
 
-// 토큰의 종류를 나타내는 열거형
 enum class TokenType {
-    // 키워드
+    // Keyword
     DEF, CLASS, STRUCT, IF, ELSE, WHILE, FOR, RETURN,
     INT32, FLOAT, BOOL, CHAR, LIST, TRUE, FALSE,
 
-    // 식별자, 리터럴
+    // Identifier, Literal
     IDENTIFIER,
     INTEGER_LITERAL,
     STRING_LITERAL,
 
-    // 연산자
+    // Operator 
     PLUS, MINUS, ASTERISK, SLASH, PERCENT,
     ASSIGN, EQUALS, NOT_EQUALS,
     LT, GT, LTE, GTE,
     INCREMENT, DECREMENT,
 
-    // 구분자
+    // Seperator
     LPAREN, RPAREN, // ( )
     LBRACE, RBRACE, // { }
     LBRACKET, RBRACKET, // [ ]
     SEMICOLON, COLON, DOT, COMMA,
 
-    // 기타
-    ILLEGAL, // 알 수 없는 토큰
+    // ETC
+    ILLEGAL,
     END_OF_FILE
 };
 
-// 토큰 구조체
 struct Token {
     TokenType type;
     std::string value;
